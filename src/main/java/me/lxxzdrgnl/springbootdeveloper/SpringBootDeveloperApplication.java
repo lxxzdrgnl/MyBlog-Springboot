a84c1e54264c8343e430a3d569d4f80a8a37964b
lxxzdrgnl <pung4905@naver.com>
2025-02-24 21:05:24 +0900

    upload

--- src/main/java/me/lxxzdrgnl/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/lxxzdrgnl/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootDeveloperApplication {
+public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootDeveloperApplication.class, args);
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
