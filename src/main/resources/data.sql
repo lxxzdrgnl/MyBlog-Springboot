13d0bb2d0ea8de68577fae571f2e49ed3595380f
lxxzdrgnl <pung4905@naver.com>
2025-02-26 20:56:19 +0900

    upload

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO member (id, name) VALUES (1, '유다연')
-INSERT INTO member (id, name) VALUES (2, '이용재')
+INSERT INTO member (id, name) VALUES (1, 'name 1')
+INSERT INTO member (id, name) VALUES (2, 'name 2')
 INSERT INTO member (id, name) VALUES (3, 'name 3')
