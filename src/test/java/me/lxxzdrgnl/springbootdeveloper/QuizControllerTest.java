5f7f82b81591f1587591f272b90a8f37318d56db
lxxzdrgnl <pung4905@naver.com>
2025-03-14 22:45:43 +0900

    upload

--- src/test/java/me/lxxzdrgnl/springbootdeveloper/QuizControllerTest.java
+++ src/test/java/me/lxxzdrgnl/springbootdeveloper/QuizControllerTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -63,7 +65,14 @@ public class QuizControllerTest {
     @DisplayName("quiz(): POST /quiz?code=1 이면 응답 코드는 403, 응답 본문은 Forbidden!를 리턴한다.")
     @Test
     public void postQuiz1() throws Exception {
-        // given
+        //given
         final String url = "/quiz";
-
+        //when
+        final ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new Code(1)))
+        );
+        result
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("Forbidden!"));
 }
