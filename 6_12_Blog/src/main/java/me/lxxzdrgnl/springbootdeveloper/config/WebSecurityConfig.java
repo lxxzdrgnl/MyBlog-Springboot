d9d4c5694518abfd87f8a0c37b198505b5a6ae9c
lxxzdrgnl <pung4905@naver.com>
2025-07-02 21:04:36 +0900

    upload

---
+++ 6_12_Blog/src/main/java/me/lxxzdrgnl/springbootdeveloper/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package me.lxxzdrgnl.springbootdeveloper.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public WebSecurityCustomizer configure(){
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+    }
+}
